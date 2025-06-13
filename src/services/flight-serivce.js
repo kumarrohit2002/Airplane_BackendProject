9899c36d09547f0fce675d0c04f19225c160d9c6
rohit Kumar <rohitranjanrrsingh@gmail.com>
2025-06-13 14:46:51 +0530

    fix:Departure time must be before arrival time in Create Flight in create Api

--- src/services/flight-serivce.js
+++ src/services/flight-serivce.js
@@ -3,13 +3,21 @@ const {FlightRepository} =require('../repositories');
 const flightRepository=new FlightRepository();
 const AppError= require('../utils/errors/app-error');
 const {StatusCodes} = require('http-status-codes');
+const {compareTime}=require('../utils/helper/datetime-helper');
 
 
 async function createFlight(data){
     try{
+        const {departureTime,arrivalTime}=data;
+        if(compareTime(departureTime,arrivalTime)){ //departure < arrival
+            throw new AppError(['Departure time must be before arrival time'], StatusCodes.BAD_REQUEST);
+        }
         const flight=await flightRepository.create(data);
         return flight;
     }catch(error){
+        if (error instanceof AppError) {
+            throw error;
+        }
         if(error.name == 'SequelizeValidationError'){
             let explanation=[];
             error.errors.forEach((err)=>{
